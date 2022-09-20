0435b93ef05227d545886419b35b3c9846210df4
Alexeybroadway <johnclevervoice@gmail.com>
2022-09-20 19:13:46 +0500

    Переменные homework

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,21 @@ public class Main {
         var dog = 8;
         System.out.println(dog);
         var cat = 3.6;
-        System.out.println(3.6);
-        var paper = 763789;
-        System.out.println(763789);
+        System.out.println(cat);
+        var papper = 763789;
+        System.out.println(papper);
+        dog = dog + 4;
+        System.out.println(dog);
+        cat = cat + 4;
+        System.out.println(cat);
+        papper = papper + 4;
+        System.out.println(papper);
+        dog = dog - 3.5;
+        System.out.println(dog);
+        cat = cat - 1.6;
+        System.out.println(cat);
+        papper = papper - 7639;
+        System.out.println(papper);
+
     }
 }
